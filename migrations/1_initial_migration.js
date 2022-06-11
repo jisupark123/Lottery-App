9387821d9a07b48f51a19ac1a94c948b646f0b19
박지수 <jisupark2000@gmail.com>
2022-06-12 01:20:42 +0900

    config basic

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+// build 폴더의 Migrations.js 파일을 가져옴
+const Migrations = artifacts.require('Migrations');
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
